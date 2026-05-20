2c5461e8c5079a7d3dd54d53dd38384d4709b5f2
narendramukkawar <narendramukkawar03@gmail>
2026-05-20 13:28:31 +0530

    POD011: Updating logic for transfer method

--- src/main/java/com/paymenttransactionservice/service/PaymentServiceImpl.java
+++ src/main/java/com/paymenttransactionservice/service/PaymentServiceImpl.java
@@ -2,15 +2,21 @@ package com.paymenttransactionservice.service;
 
 import com.paymenttransactionservice.dto.AccountRequestDTO;
 import com.paymenttransactionservice.dto.TransferRequestDTO;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
 
-    private final RestTemplate restTemplate = new RestTemplate();
+    private final RestTemplate restTemplate;
 
     private static final String ACCOUNT_SERVICE_URL = "http://localhost:8082/api/accounts";
+
+    public PaymentServiceImpl(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     @Override
     public void transfer(TransferRequestDTO request) {
@@ -19,35 +25,48 @@ public class PaymentServiceImpl implements PaymentService {
 
         try {
             // Create Debit Request
-            AccountRequestDTO debitRequest = new AccountRequestDTO();
-            debitRequest.setAccountId(request.getFromAccountId());
-            debitRequest.setAmount(request.getAmount());
+//            AccountRequestDTO debitRequest = new AccountRequestDTO();
+//            debitRequest.setAccountId(request.getFromAccountId());
+//            debitRequest.setAmount(request.getAmount());
 
             // Debit
-            restTemplate.postForObject(ACCOUNT_SERVICE_URL + "/debit", debitRequest, String.class);
+            //restTemplate.postForObject(ACCOUNT_SERVICE_URL + "/" + request.getToAccountId() + "/debit", debitRequest, String.class);
 
+            String debitUrl = ACCOUNT_SERVICE_URL + "/" + request.getFromAccountNumber() + "/debit?amount=" + request.getAmount();
+
+            restTemplate.exchange(debitUrl, HttpMethod.PATCH, HttpEntity.EMPTY, String.class);
             debitSuccess = true;
 
             // Create Credit Request
-            AccountRequestDTO creditRequest = new AccountRequestDTO();
-            creditRequest.setAccountId(request.getToAccountId());
-            creditRequest.setAmount(request.getAmount());
+//            AccountRequestDTO creditRequest = new AccountRequestDTO();
+//            creditRequest.setAccountId(request.getToAccountId());
+//            creditRequest.setAmount(request.getAmount());
+//
+//            // Credit
+//            restTemplate.postForObject(ACCOUNT_SERVICE_URL + "/credit", creditRequest, String.class);
 
-            // Credit
-            restTemplate.postForObject(ACCOUNT_SERVICE_URL + "/credit", creditRequest, String.class);
+            String creditUrl = ACCOUNT_SERVICE_URL + "/" + request.getToAccountNumber() + "/credit?amount=" + request.getAmount();
+
+            restTemplate.exchange(creditUrl, HttpMethod.PATCH, HttpEntity.EMPTY, String.class);
 
         } catch (Exception ex) {
 
             // Compensation (only if debit happened)
+//            if (debitSuccess) {
+//                AccountRequestDTO rollbackRequest = new AccountRequestDTO();
+//                rollbackRequest.setAccountId(request.getFromAccountId());
+//                rollbackRequest.setAmount(request.getAmount());
+//
+//                restTemplate.postForObject(ACCOUNT_SERVICE_URL + "/credit", rollbackRequest, String.class);
+//            }
             if (debitSuccess) {
-                AccountRequestDTO rollbackRequest = new AccountRequestDTO();
-                rollbackRequest.setAccountId(request.getFromAccountId());
-                rollbackRequest.setAmount(request.getAmount());
 
-                restTemplate.postForObject(ACCOUNT_SERVICE_URL + "/credit", rollbackRequest, String.class);
+                String rollbackUrl = ACCOUNT_SERVICE_URL + "/" + request.getFromAccountNumber() + "/credit?amount=" + request.getAmount();
+
+                restTemplate.exchange(rollbackUrl, HttpMethod.PATCH, HttpEntity.EMPTY, String.class);
             }
 
-            throw new RuntimeException("Transaction failed, rolled back");
+            throw new RuntimeException("Transaction failed: " + ex.getMessage());
         }
     }
 }
